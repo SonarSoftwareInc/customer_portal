186df595119d0b1f25c26752f4ed9ab052aba006
Chris Trower <chris@sonar.software>
2019-08-30 13:27:36 -0500

    Added additional currency to gocardless dropdown per gocardless documentation

--- resources/views/pages/config/show.blade.php
+++ resources/views/pages/config/show.blade.php
@@ -404,7 +404,7 @@
                      <label>
                      Currency Mode
                      </label>
-                     {!! Form::select('go_cardless_currency_code', array('EUR' => 'EUR', 'GDP' => 'GDP', 'SEK' => 'SEK'), $systemSetting->go_cardless_currency_code,['id' => 'go_cardless_currency_code', 'class' => 'form-control', 'data-toggle' => 'select']); !!}
+                     {!! Form::select('go_cardless_currency_code', array('AUD' => 'AUD', 'CAD' => 'CAD', 'DKK' => 'DKK', 'EUR' => 'EUR', 'GBP' => 'GBP', 'NZD' => 'NZD', 'SEK' => 'SEK', 'USD' => 'USD'), $systemSetting->go_cardless_currency_code,['id' => 'go_cardless_currency_code', 'class' => 'form-control', 'data-toggle' => 'select']); !!}
                   </div>
                </div>
             </div>
