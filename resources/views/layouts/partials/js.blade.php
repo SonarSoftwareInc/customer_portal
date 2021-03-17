d84e4f7aafebbcdcb0bb0496e55ad710dae682a5
80at8 <andrew@tejero.ca>
2021-03-17 22:11:33 +0000

    changed to classname

--- resources/views/layouts/partials/js.blade.php
+++ resources/views/layouts/partials/js.blade.php
@@ -1,5 +1,5 @@
 <script nonce="{{ csp_nonce() }}">
-close=document.getElementById("close");close.addEventListener('click',function(){close.className="csp_style3"; setTimeout(function(){ close.className="csp_style1"; }, 600); },false);
+close=document.getElementById("close");close.addEventListener('click',function(){close.className.Add("csp_style3"); setTimeout(function(){ close.className,Add("csp_style1"); }, 600); },false);
    var _portal = {
        currencySymbol: '{{Config::get("customer_portal.currency_symbol")}}',
        thousandsSeparator: '{{Config::get("customer_portal.thousands_separator")}}',
