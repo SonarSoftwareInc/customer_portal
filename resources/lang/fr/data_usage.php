370d10c8f7effdaf24889fbc1a18a1c265d8644c
Gene Chen <gene@sonar.software>
2021-06-09 15:16:07 -0700

    Fix whitespaces

--- resources/lang/fr/data_usage.php
+++ resources/lang/fr/data_usage.php
@@ -8,9 +8,9 @@ return [
     'purchaseAdditionalData' => 'Acheter des données additionnelles',
     'quantity' => 'Quantité',
     'topOffExplanation' => 'Combien d’unités additionnelles aimeriez-vous acheter :Prix par :unitsGB?',
-    'topOffTotal' => 'Votre limite sera augmentée  :countGB le prix sera  :prix',
+    'topOffTotal' => 'Votre limite sera augmentée :countGB le prix sera :prix',
     'confirmTopOffAddition' => 'Ajouter les données additionnelles et facturer mon compte',
     'successfullyAddedUsage' => ' Votre capacité pour cette période de facturation a été augmentée!',
-    'youMustHaveACardOnFile' => 'Vous devez avoir une carte de crédit  à votre dossier pour acheter des données additionnelles. Ajouter une carte.',
+    'youMustHaveACardOnFile' => 'Vous devez avoir une carte de crédit à votre dossier pour acheter des données additionnelles. Ajouter une carte.',
     'monthlyGraphHeader' => 'Ce graphique illustre votre utilisation mensuelle totale des douze périodes de facturation précédentes.',
 ];
