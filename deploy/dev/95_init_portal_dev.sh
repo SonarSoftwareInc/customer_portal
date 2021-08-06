91f75760ce7c7017ed2d6abc0f6de090dde3862e
Jae Muzzin <jae@sonar.software>
2021-08-06 11:32:19 -0400

    unod

--- deploy/dev/95_init_portal_dev.sh
+++ deploy/dev/95_init_portal_dev.sh
@@ -5,7 +5,7 @@ if [ "${APP_ENV-}" = "local" ]
 then
     envsubst \$NGINX_HOST < /etc/nginx/conf.d/customerportal-dev.template > /etc/nginx/sites-available/default
     sed "/$OKTETO_SONAR_HOST$/d" /etc/hosts > /tmp/newhosts
-    echo "$OKTETO_SONAR_HOST" >> /tmp/newhosts
+    echo "$WEB_SERVICE_HOST $OKTETO_SONAR_HOST" >> /tmp/newhosts
     cp /tmp/newhosts /etc/hosts
     rm /tmp/newhosts
 fi
