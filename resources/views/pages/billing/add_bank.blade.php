2e2fdc1aedcb6889f2714566b2fade61143e7608
Jae Muzzin <jae@sonar.software>
2021-08-10 12:44:59 -0400

    add tracker ids

--- resources/views/pages/billing/add_bank.blade.php
+++ resources/views/pages/billing/add_bank.blade.php
@@ -112,6 +112,7 @@
         </div>
         <div class="row">
             <div class="col-12 col-md-12">
+                <input type="hidden" name="payment_tracker_id" value="{{uniqid("", true)}}" />
                 <button type="submit" class="btn btn-primary">{{utrans("billing.addNewBankAccount")}}</button>
                 {!! Form::close() !!}
             </div>
