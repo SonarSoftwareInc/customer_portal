9e068cf7dbe319d9c43f2c04135758bc5e690863
Brett Bowles <brett.bowles@gmail.com>
2023-06-12 14:34:34 -0400

    https back to http in app.php

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
