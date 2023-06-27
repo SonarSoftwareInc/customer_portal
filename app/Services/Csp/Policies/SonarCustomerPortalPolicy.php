c65b3240aeb3c60bd0f967344f5733e644d69bdf
Tristan Shelton <tristan@sonar.software>
2023-06-26 17:44:45 -0700

    Fix CSP for updated dependencies

--- app/Services/Csp/Policies/SonarCustomerPortalPolicy.php
+++ app/Services/Csp/Policies/SonarCustomerPortalPolicy.php
@@ -9,11 +9,23 @@ use Spatie\Csp\Value;
 
 class SonarCustomerPortalPolicy extends Basic
 {
-    public function configure()
-    {
-        parent::configure();
+	public function configure()
+	{
+  		$this
+            // Duplicated from `Basic` because nonce overrides
+            // the `unsafe-inline` for styles (see below) and
+            // there's no `removeNonceForDirective`.
+            ->addDirective(Directive::BASE, Keyword::SELF)
+            ->addDirective(Directive::CONNECT, Keyword::SELF)
+            ->addDirective(Directive::DEFAULT, Keyword::SELF)
+            ->addDirective(Directive::FORM_ACTION, Keyword::SELF)
+            ->addDirective(Directive::IMG, Keyword::SELF)
+            ->addDirective(Directive::MEDIA, Keyword::SELF)
+            ->addDirective(Directive::OBJECT, Keyword::NONE)
+            ->addDirective(Directive::SCRIPT, Keyword::SELF)
+            ->addDirective(Directive::STYLE, Keyword::SELF)
+            ->addNonceForDirective(Directive::SCRIPT)
 
-        $this
             ->addDirective(Directive::FRAME, [
                 'self',
                 'js.stripe.com',
@@ -33,9 +45,10 @@ class SonarCustomerPortalPolicy extends Basic
                 'js.stripe.com',
             ])
 
-            ->addDirective(Directive::STYLE, [
-                'self',
-            ])
+			->addDirective(Directive::STYLE, [
+				'self',
+                'unsafe-inline', // Required for TinyMCE https://www.tiny.cloud/docs/tinymce/6/tinymce-and-csp/
+			])
 
             ->addDirective(Directive::CONNECT, [
                 'self',
