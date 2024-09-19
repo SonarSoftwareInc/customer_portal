b67821cdbe60d270855494ad765fa1992dc1a830
avative-pat <pat@avative.com>
2024-09-18 21:18:19 -0600

    Adding Spanish locale

---
+++ lang/es/actions.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'login' => 'Iniciar Sesión',
+    'makePayment' => 'Realizar Pago',
+    'createTicket' => 'Crear Ticket',
+    'lookupEmail' => 'Buscar Dirección de Correo Electrónico',
+    'createAccount' => 'Crear Cuenta',
+    'sendResetEmail' => 'Enviar Correo de Restablecimiento',
+    'postReply' => 'Publicar una Respuesta',
+    'delete' => 'Eliminar',
+    'resetPassword' => 'Restablecer Contraseña',
+    'loginMessage' => 'Accede a tu cuenta de :ispName',
+    'linkMessage' => 'Vincula tu cuenta de :ispName',
+];
