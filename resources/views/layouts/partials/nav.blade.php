66949c5e2c9a28b59fb0002837c69a12accecc66
HellooooNewman <kevin@kevinnewman.ca>
2025-02-20 01:49:15 -0500

    Fixing mobile menu selector

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,14 +1,14 @@
 <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidebar">
    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebarCollapse" aria-controls="sidebarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarCollapse" aria-controls="sidebarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
       </button>
       <a class="navbar-brand" href="{{action([\App\Http\Controllers\BillingController::class, 'index'])}}">
-      <img src="/assets/img/logo.png" class="navbar-brand-img mx-auto" alt="Back to dashboard...">
+         <img src="/assets/img/logo.png" class="navbar-brand-img mx-auto" alt="Back to dashboard...">
       </a>
       <div class="navbar-user d-md-none">
          <div class="dropdown">
-            <a href="#!" id="sidebarIcon" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a href="#!" id="sidebarIcon" class="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                <div class="avatar avatar-sm avatar-online">
                   <img src="/assets/img/alec.png" class="avatar-img rounded-circle" alt="...">
                </div>
@@ -29,24 +29,24 @@
          <ul class="navbar-nav">
             <li class="nav-item">
                <a @if(str_contains(Route::getCurrentRoute()->uri(),"billing")) class="nav-link selected" @else class="nav-link" @endif href="{{action([\App\Http\Controllers\BillingController::class, 'index'])}}">
-               <i class="fe fe-dollar-sign"></i> {{utrans("nav.billing")}}</a>
+                  <i class="fe fe-dollar-sign"></i> {{utrans("nav.billing")}}</a>
             </li>
             @if(config("customer_portal.ticketing_enabled") === true)
             <li class="nav-item">
                <a @if(str_contains(Route::getCurrentRoute()->uri(),"tickets")) class="nav-link selected" @else class="nav-link" @endif href="{{action([\App\Http\Controllers\TicketController::class, 'index'])}}">
-               <i class="fe fe-message-circle"></i> {{utrans("nav.tickets")}}</a>
+                  <i class="fe fe-message-circle"></i> {{utrans("nav.tickets")}}</a>
             </li>
             @endif
             @if(config("customer_portal.data_usage_enabled") === true)
             <li class="nav-item">
                <a @if(str_contains(Route::getCurrentRoute()->uri(),"data_usage")) class="nav-link selected" @else class="nav-link" @endif href="{{action([\App\Http\Controllers\DataUsageController::class, 'index'])}}">
-               <i class="fe fe-activity"></i> {{utrans("nav.dataUsage")}}</a>
+                  <i class="fe fe-activity"></i> {{utrans("nav.dataUsage")}}</a>
             </li>
             @endif
             @if(config("customer_portal.contracts_enabled") === true)
             <li class="nav-item">
                <a @if(str_contains(Route::getCurrentRoute()->uri(),"contracts")) class="nav-link selected" @else class="nav-link" @endif href="{{action([\App\Http\Controllers\ContractController::class, 'index'])}}">
-               <i class="fe fe-package"></i> {{utrans("nav.contracts")}}</a>
+                  <i class="fe fe-package"></i> {{utrans("nav.contracts")}}</a>
             </li>
             @endif
          </ul>
@@ -56,18 +56,18 @@
          <ul class="navbar-nav">
             <li class="nav-item">
                <a @if(str_contains(Route::getCurrentRoute()->uri(),"profile")) class="nav-link selected" @else class="nav-link" @endif href="{{action([\App\Http\Controllers\ProfileController::class, 'show'])}}">
-               <i class="fe fe-users"></i> {{utrans("nav.profile")}}</a>
+                  <i class="fe fe-users"></i> {{utrans("nav.profile")}}</a>
             </li>
             <li class="nav-item">
                <a class="nav-link" href="/logout">
-               <i class="fe fe-log-out"></i> {{utrans("nav.logOut")}}</a>
+                  <i class="fe fe-log-out"></i> {{utrans("nav.logOut")}}</a>
             </li>
-@if(str_contains(Route::getCurrentRoute()->uri(),"settings"))
+            @if(str_contains(Route::getCurrentRoute()->uri(),"settings"))
             <li class="nav-item">
                <a class="nav-link selected" href="{{action([\App\Http\Controllers\AppConfigController::class, 'show'])}}">
-               <i class="fe fe-codepen"></i> App Configuration</a>
+                  <i class="fe fe-codepen"></i> App Configuration</a>
             </li>
-@endif
+            @endif
 
          </ul>
          <div class="navbar-user mt-auto d-none d-md-flex">
