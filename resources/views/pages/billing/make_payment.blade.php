a85705558aaf871c2c2c8c7df54854ccc9550c0e
HellooooNewman <kevin@kevinnewman.ca>
2025-05-12 14:17:47 -0400

    Use singular string instead

--- resources/views/pages/billing/make_payment.blade.php
+++ resources/views/pages/billing/make_payment.blade.php
@@ -249,8 +249,7 @@
                </div>
             </div>
             <div class="col mt-1 credit-card-autopay">
-               <strong>{{utrans("billing.enableAutomaticPayments")}}</strong>
-               {{utrans("billing.saveAsAutoPayMethod")}}
+               {!! utrans("billing.saveAsAutoPayMethod") !!}
             </div>
             <div class="col mt-1 bank-account-payment">
                {{utrans("billing.authorizePaymentAccount", ["business_name" => config("customer_portal.company_name"), "dateToday" => $dateToday])}}
